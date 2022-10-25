17e9438c061ec9b436bd9d1c9bb14bfdc4b2f656
pepiyu <58489884+pepiyu@users.noreply.github.com>
2022-10-25 20:41:41 +0200

    ✨ feat random videos

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,30 +1,30 @@
-import { FaBookReader, FaSeedling, FaDog, FaLaptopCode, FaWalking, FaVolumeUp, FaVolumeMute } from "react-icons/fa";
+import { FaBookReader, FaSeedling, FaDog, FaLaptopCode, FaWalking, FaVolumeUp, FaVolumeMute, FaClock, FaRegListAlt } from "react-icons/fa";
 
-const Navbar = ({setReproduction, mute, setMute}) => {
+const Navbar = ({setReproduction, mute, setMute, setTimerVisible, setToDoListVisible}) => {
 
     const videoContent = [
         {
-          id: '9aJsVTBDeyA',
+          id: ['9aJsVTBDeyA','a3dGN1K_Y0E','6MsRcPRP0DU','RYc4ouHyEvM'],
           name: 'Study with me',
           icon: <FaBookReader/>
         },
         {
-          id: 'b4AkjHqDDK8',
+          id: ['b4AkjHqDDK8','IjJ_uuLn0PQ'],
           name: 'Nature',
           icon: <FaSeedling/>
         },
         {
-          id: 'Q2nrqFQ5YyQ',
+          id: ['Q2nrqFQ5YyQ','HjvIAJMbXi4','_E4RmEMKANQ'],
           name: 'Animal',
           icon: <FaDog/>
         },
         {
-          id: 'jF9kUaxHtB4',
+          id: ['jF9kUaxHtB4','Pn2mzyU52yI','Nxb57R-HK1I','jUoYSqxmulw'],
           name: 'Code with me',
           icon: <FaLaptopCode/>
         },
         {
-          id: 'sPtuOcFmLOs',
+          id: ['sPtuOcFmLOs','vWeMvNMA7Co'],
           name: 'Walk with me',
           icon: <FaWalking/>
         },
@@ -32,7 +32,9 @@ const Navbar = ({setReproduction, mute, setMute}) => {
 
     const handleOnClick = (n) => {
 
-        setReproduction(videoContent[n])
+        const random = Math.floor(Math.random() * videoContent[n].id.length);
+
+        setReproduction({id: videoContent[n].id[random], name: videoContent[n].name})
 
     }
        
@@ -49,18 +51,40 @@ const Navbar = ({setReproduction, mute, setMute}) => {
                             <div className="navbar-icon">
                                 {video.icon}
                             </div>
-                            <span>{video.name}</span>
+                            <span className="navbar-text">{video.name}</span>
                         </div>
                     )
                 })}
 
+                <hr className="divisor"></hr>
+
                 <div>
-                    <div className="navbar-icon" onClick={() => setMute(!mute)}>
-                        {mute ? <FaVolumeMute/> : <FaVolumeUp/>}
+                    <div className="navbar-item" onClick={() => setMute(!mute)}>
+                        <div className="navbar-icon" >
+                            {mute ? <FaVolumeMute/> : <FaVolumeUp/>}
+                        </div>
+                        <span className="navbar-text">{mute ? "Mute" : "On"}</span>
                     </div>
+
+                    
 
 
                 </div>
+
+                <div className="navbar-item" onClick={() => setTimerVisible(true)}>
+                    <div className="navbar-icon">
+                        <FaClock/>
+                    </div>
+                    <span className="navbar-text">Timer</span>
+                </div>
+
+                <div className="navbar-item" onClick={() => setToDoListVisible(true)}>
+                    <div className="navbar-icon">
+                        <FaRegListAlt/>
+                    </div>
+                    <span className="navbar-text">To-do</span>
+                </div>
+
             </div>
         
         </div>
