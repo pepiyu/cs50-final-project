700d399b12203fba1155657bb34a5689311601d2
pepiyu <58489884+pepiyu@users.noreply.github.com>
2022-10-25 20:39:36 +0200

    ✨ new feat: react draggable

--- src/components/Desktop/index.js
+++ src/components/Desktop/index.js
@@ -1,13 +1,40 @@
+import {useState} from 'react'
 import Navbar from '../Navbar';
 import Timer from '../Timer';
 import ToDoList from '../ToDoList';
+import Draggable from 'react-draggable'; // Both at the same time
 
-const Desktop = ({setReproduction, mute, setMute}) => {
+const Desktop = ({setReproduction, mute, setMute, timerVisible, setTimerVisible, toDoListVisible, setToDoListVisible}) => {
+
+
     return (
         <div className="desktop">
-            <Navbar setReproduction={setReproduction} mute={mute} setMute={setMute}/>
-            <Timer/>
-            <ToDoList/>
+            <Navbar 
+            setReproduction={setReproduction} 
+            mute={mute} 
+            setMute={setMute}
+            setTimerVisible={setTimerVisible}
+            setToDoListVisible={setToDoListVisible}
+        
+            
+            />
+            <Draggable>
+                <div>
+                    <Timer
+                    timerVisible={timerVisible}
+                    setTimerVisible={setTimerVisible}
+                    />
+                </div>
+            </Draggable>
+            <Draggable>
+                <div>
+                <ToDoList
+                toDoListVisible={toDoListVisible}
+                setToDoListVisible={setToDoListVisible}
+                
+                />
+                </div>
+            </Draggable>
 
         </div>
     );
