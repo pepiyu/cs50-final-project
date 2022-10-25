0777e603f743aaa2d7f9c769b00672017652fe50
pepiyu <58489884+pepiyu@users.noreply.github.com>
2022-10-25 20:41:22 +0200

    ✨ feat minimize function

--- src/App.js
+++ src/App.js
@@ -12,13 +12,25 @@ function App() {
     }
     );
 
+  const [timerVisible, setTimerVisible] = useState(true)
+  const [toDoListVisible, setToDoListVisible] = useState(true)
+
+
   const [ mute, setMute ] = useState(true)
 
   return (
     <div className="App">
       <main className="fullscreen">
         <Workspace reproduction={reproduction} mute={mute}/>
-        <Desktop setReproduction={setReproduction} mute={mute} setMute={setMute} />
+        <Desktop 
+        setReproduction={setReproduction} 
+        mute={mute} 
+        setMute={setMute}
+        timerVisible={timerVisible}
+        setTimerVisible={setTimerVisible}
+        toDoListVisible={toDoListVisible}
+        setToDoListVisible={setToDoListVisible}
+        />
         
       </main>
 
